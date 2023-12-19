0d8eb5f67b0fa9db3d77374b6925dcacb377d555
JackStrick <jonstrickland96@gmail.com>
2023-12-19 11:37:38 -0500

    update comments

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,4 @@
-// Import necessary dependencies from Redux Toolkit
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-
 // Import your Redux reducers from their respective locations
 import authReducer from "../features/auth/authSlice";
 import modeReducer from "../features/mode/modeSlice";
@@ -13,7 +11,7 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 // Define the middleware array configuration
 const middleware = [...getDefaultMiddleware()];
 
-// Configure and create your Redux store
+// Configure and create Redux store
 export const store = configureStore({
 	reducer: {
 		// Define your Redux reducers
